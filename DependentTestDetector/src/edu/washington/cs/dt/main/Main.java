f825bd28114475e4072919261b6e95cd7cf76b85
racezhang@gmail.com <racezhang@gmail.com@44adb83a-8287-3463-537c-1c5ef794c0d2>
2012-02-24 02:35:26 +0000

    add some comments
    git-svn-id: http://testisolation.googlecode.com/svn/trunk@11 44adb83a-8287-3463-537c-1c5ef794c0d2

--- DependentTestDetector/src/edu/washington/cs/dt/main/Main.java
+++ DependentTestDetector/src/edu/washington/cs/dt/main/Main.java
@@ -4,5 +4,13 @@ package edu.washington.cs.dt.main;
  * The entry point
  * */
 public class Main {
-
+	
+	/**
+	 * Basic workflow:
+	 * input: a file containing all junit tests
+	 *        specify classpath
+	 *        specify tmpOutputFile
+	 * output: a report output file (default by print)
+	 * */
+	
 }
