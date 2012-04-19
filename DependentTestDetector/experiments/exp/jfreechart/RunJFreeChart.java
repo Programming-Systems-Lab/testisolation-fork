27f3b135cf6dea4131c58bfa53f87785aa863d50
racezhang@gmail.com <racezhang@gmail.com@44adb83a-8287-3463-537c-1c5ef794c0d2>
2012-04-19 19:20:15 +0000

    Upload test driver for jfreechart, and also upload the needed jars.
    git-svn-id: http://testisolation.googlecode.com/svn/trunk@73 44adb83a-8287-3463-537c-1c5ef794c0d2

---
+++ DependentTestDetector/experiments/exp/jfreechart/RunJFreeChart.java
@@ -0,0 +1,19 @@
+package exp.jfreechart;
+
+import edu.washington.cs.dt.main.Main;
+import edu.washington.cs.dt.util.Log;
+import junit.framework.TestCase;
+
+public class RunJFreeChart extends TestCase {
+	
+	public void testRunInIsolation() {
+		Log.logConfig("./log-jfreechart.txt");
+		String inputTestFile = "./experiments-subjects/jfreechart/jfreechart-junittests.txt";
+		//please update the whole workspace, I add a folder containing necessary jars
+		//for the experiments.
+		//make sure the jars under ./experiments-subjects/jfreechart are added to classpath
+		Main.main(new String[]{"--minimize=false", "--isolate",
+				"--tests=" + inputTestFile, "--showProgress", "--printstacktrace"});
+	}
+	
+}
