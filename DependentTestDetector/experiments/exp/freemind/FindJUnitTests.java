f50ca04c77154bbc6a83925852c800f8be36e6f0
racezhang@gmail.com <racezhang@gmail.com@44adb83a-8287-3463-537c-1c5ef794c0d2>
2012-04-27 17:22:42 +0000

    Drivers to find unit tests from freemind.jar and run the tests
    git-svn-id: http://testisolation.googlecode.com/svn/trunk@85 44adb83a-8287-3463-537c-1c5ef794c0d2

---
+++ DependentTestDetector/experiments/exp/freemind/FindJUnitTests.java
@@ -0,0 +1,26 @@
+package exp.freemind;
+
+import java.io.IOException;
+import java.util.zip.ZipException;
+
+import junit.framework.TestCase;
+
+import edu.washington.cs.dt.tools.UnitTestFinder;
+import edu.washington.cs.dt.util.Log;
+
+public class FindJUnitTests extends TestCase {
+	
+	public void testFindUnitTests() throws ZipException, ClassNotFoundException, IOException {
+		Log.logConfig("./freemind-findtests-log.txt");
+		/**
+		 * Make sure all jars under folder: experiments-subjects/freemind (including those
+		 * in lib) are added to classpath  before running this test.
+		 * */
+		String[] args = new String[]{
+				"--pathOrJarFile=./experiments-subjects/freemind/freemind.jar",
+				"--outputFileName=./experiments-subjects/freemind/freemind-unit-tests.txt"
+		};
+		UnitTestFinder.main(args);
+	}
+	
+}
---
+++ DependentTestDetector/experiments/exp/freemind/RunFreemindTests.java
@@ -0,0 +1,18 @@
+package exp.freemind;
+
+import edu.washington.cs.dt.main.Main;
+import edu.washington.cs.dt.util.Log;
+import junit.framework.TestCase;
+
+public class RunFreemindTests extends TestCase {
+	public void testRunInIsolation() {
+		Log.logConfig("./log-freemind.txt");
+		String inputTestFile = "./experiments-subjects/freemind/freemind-unit-tests.txt";
+		/**
+		 * Make sure all jars under folder: experiments-subjects/freemind (including those
+		 * in lib) are added to classpath  before running this test.
+		 * */
+		Main.main(new String[]{"--minimize=false", "--isolate",
+				"--tests=" + inputTestFile, "--showProgress", "--printstacktrace"});
+	}
+}
