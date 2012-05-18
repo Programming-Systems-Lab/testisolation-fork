6c29714048ba71c117cd6eecd9aa6c3199b16e54
racezhang@gmail.com <racezhang@gmail.com@44adb83a-8287-3463-537c-1c5ef794c0d2>
2012-05-18 18:43:24 +0000

    rename
    git-svn-id: http://testisolation.googlecode.com/svn/trunk@108 44adb83a-8287-3463-537c-1c5ef794c0d2

--- DependentTestDetector/tests/edu/washington/cs/dt/samples/SampleJUnit4Tests.java
+++ DependentTestDetector/tests/edu/washington/cs/dt/samples/SampleJUnit4Tests.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 
 public class SampleJUnit4Tests {
 	@Test
-	public void testJUnit4_1() {}
+	public void aJUnit4_1() {}
 	@Test
-	public void testJUnit4_2() {}
+	public void aJUnit4_2() {}
 	@Test
-	public void testJUnit4_3() {}
+	public void aJUnit4_3() {}
 }
