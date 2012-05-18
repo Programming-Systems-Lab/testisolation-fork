03d919aa7d57edf1639e0dd724acb495321d2d65
racezhang@gmail.com <racezhang@gmail.com@44adb83a-8287-3463-537c-1c5ef794c0d2>
2012-05-18 18:42:43 +0000

    2 junit tests
    git-svn-id: http://testisolation.googlecode.com/svn/trunk@107 44adb83a-8287-3463-537c-1c5ef794c0d2

---
+++ DependentTestDetector/tests/edu/washington/cs/dt/samples/SampleJUnit3Tests.java
@@ -0,0 +1,11 @@
+package edu.washington.cs.dt.samples;
+
+import junit.framework.TestCase;
+
+public class SampleJUnit3Tests extends TestCase {
+
+	public void testJUnit3_1() {}
+	public void testJUnit3_2() {}
+	public void testJUnit3_3() {}
+	
+}
---
+++ DependentTestDetector/tests/edu/washington/cs/dt/samples/SampleJUnit4Tests.java
@@ -0,0 +1,12 @@
+package edu.washington.cs.dt.samples;
+
+import org.junit.Test;
+
+public class SampleJUnit4Tests {
+	@Test
+	public void testJUnit4_1() {}
+	@Test
+	public void testJUnit4_2() {}
+	@Test
+	public void testJUnit4_3() {}
+}
