2c6fa2912ce21389f6abce4dba727e8d43ea06c2
racezhang@gmail.com <racezhang@gmail.com@44adb83a-8287-3463-537c-1c5ef794c0d2>
2012-06-07 00:46:24 +0000

    write tests for analysis
    git-svn-id: http://testisolation.googlecode.com/svn/trunk@129 44adb83a-8287-3463-537c-1c5ef794c0d2

--- DependentTestDetector/experiments/exp/synoptic/RunSynopticTests.java
+++ DependentTestDetector/experiments/exp/synoptic/RunSynopticTests.java
@@ -3,8 +3,13 @@
  */
 package exp.synoptic;
 
+import java.util.Collection;
+import java.util.Set;
+
 import junit.framework.TestCase;
+import edu.washington.cs.dt.ClusterAnalyzer;
 import edu.washington.cs.dt.main.Main;
+import edu.washington.cs.dt.tools.TestAccessingFieldsCollector;
 import edu.washington.cs.dt.util.Log;
 
 public class RunSynopticTests extends TestCase {
@@ -15,10 +20,24 @@ public class RunSynopticTests extends TestCase {
 		 * Make sure all jars under folder: experiments-subjects/synoptic (including those
 		 * in lib) are added to classpath  before running this test.
 		 * */
-//		Main.main(new String[]{"--minimize=false", "--isolate",
-//				"--tests=" + inputTestFile, "--showProgress", "--printstacktrace"});
-		
-		Main.main(new String[]{"--minimize=false", "--combination", "--k=2",
+		Main.main(new String[]{"--minimize=false", "--isolate",
 				"--tests=" + inputTestFile, "--showProgress", "--printstacktrace"});
+		
+//		Main.main(new String[]{"--minimize=false", "--combination", "--k=2",
+//				"--tests=" + inputTestFile, "--showProgress", "--printstacktrace"});
+	}
+	
+	public void testCollectFieldAccesses() {
+		String inputTestFile = "./experiments-subjects/synoptic/synoptic-unit-tests.txt";
+		String[] args = new String[]{
+				"--testFile", inputTestFile};
+		TestAccessingFieldsCollector.main(args);
+	}
+	
+	public void testFindCluster() {
+		String dir = "./output";
+		ClusterAnalyzer analyzer = new ClusterAnalyzer(dir);
+		Collection<Set<String>> colls = analyzer.generateClusters();
+		analyzer.showBasicInfo(colls);
 	}
 }
