9f8eee7212c35e6677a4aaba9127410f0430262d
wuttkej@gmail.com <wuttkej@gmail.com@44adb83a-8287-3463-537c-1c5ef794c0d2>
2012-07-09 23:25:01 +0000

    added wrapper script to run on cluster
    git-svn-id: http://testisolation.googlecode.com/svn/trunk@134 44adb83a-8287-3463-537c-1c5ef794c0d2

---
+++ DependentTestDetector/resources/scripts/run-all-experiments.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+#Script: run-all-experiments.sh
+#Purpose: Wrapper around our analysis tools that will create a 
+#         separate cluster process for each unique test combination
+#         and submit it to job control.
+#Project: Dependent test detection
+#Author: Jochen Wuttke	wuttkej@gmail.com
+#Date: 2012-07-09
+#Version: 1
+#
+# Arguments: 
+#    the JAR file to be analysed
+#    a positive (>0) integer value
+
+
+cluster_command=""
+get_test_command="java -cp dt-detector-0.2.0.jar edu.washington.cs.dt.tools.UnitTestFinder --pathOrJarFile $1 --junit3and4=true"
+dt_command="java -cp dt-detector-0.2.0.jar:$1 edu.washington.cs.dt.tools.RunTestInFixedOrder "
+c_script=resources/scripts/create_input_files.py
+
+#first, create all the directories and input files
+mkdir input
+mkdir output
+$get_test_command
+python $c_script -i allunittests.txt -k $2
+
+#loop over all input files and launch process
+for file in `ls input/`
+do
+  echo Running on $file
+  $cluster_command $dt_command --testFile input/$file --outputFile output/`basename $file .input`.output
+done
