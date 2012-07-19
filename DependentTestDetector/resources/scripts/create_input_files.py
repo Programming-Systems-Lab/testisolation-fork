07f92d050e697f4ab616e1e2433980c5a8f6fa4d
wuttkej@gmail.com <wuttkej@gmail.com@44adb83a-8287-3463-537c-1c5ef794c0d2>
2012-07-19 23:42:09 +0000

    added arguments to create limited sets of inputs
    git-svn-id: http://testisolation.googlecode.com/svn/trunk@141 44adb83a-8287-3463-537c-1c5ef794c0d2

--- DependentTestDetector/resources/scripts/create_input_files.py
+++ DependentTestDetector/resources/scripts/create_input_files.py
@@ -7,13 +7,32 @@
 # Date: 2012-06-17
 #
 # Usage:
-# create_input_files.py -i test.file -k limit
+# create_input_files.py -i test.file -K limit
 # test.file is the file containing all test names
 # the integer limit determines how long the generated sequences are
+#
+# Arguments:
+# -k        The minimun length of tuples in the output files (optional, defaults to 1)
+# -K        The maximum length of tuples in the output files. Mandatory.
+# -i        The name of the file containing all test names
+# -s        The number of files to output with this run of the program
+# -S        The first tuple to output.
+#
+# Use -s and -S together to break generating input files into several steps.
+# For example use:
+# create_input_files.py -k 3 -K 3 -s 10000 -i input.names
+# <run analysis on all inputs>
+# create_input_files.pyt -k 3 -K 3 -s 10000 -S 10001 -i input.names
+# 
+# This will create up to 10000 input tuples in the first run, and create
+# another 10000 tuples in the second run, starting with the first tuple that 
+# logically follows in the sequence.
+
 
 import argparse
 import os
 from os.path import exists
+import sys
 
 def write_file(names, i):
     outfile = open( "input/" + str(i) + ".input", "w" )
@@ -21,17 +40,21 @@ def write_file(names, i):
         outfile.write(n)
     outfile.close()
 
-def create_permutations(depth, lower, upper, tests, current, iteration):
+def create_permutations(depth, lower, upper, tests, current, iteration, first, limit):
     #print "Creating permutations for tests " + str(depth) + ":" + str(limit)
     #print current
+    if ( iteration >=  (first + limit) ): 
+#        print "Exceeded limit: " + str(iteration) + " > " + str(first) + " + " + str(limit)
+        return iteration
     it = iteration
     for t in tests:
         if ( not current.__contains__(t) ): 
             if ( depth < upper ): 
                 current.append(t)
-                if ( len(current) >= lower ): write_file(current, it)
-                it = create_permutations(depth+1, lower, upper, tests, current, it + 1 )
+                if ( len(current) >= lower and it >= first): write_file(current, it)
+                it = create_permutations(depth+1, lower, upper, tests, current, it + 1, first, limit )
                 current.remove(t)
+                if ( it >= first + limit): return it
     return it
 
 def parse_arguments():
@@ -39,13 +62,15 @@ def parse_arguments():
     parser.add_argument("-i", "--input-file", dest="IN_FILE", required=True, help="The file containing all tests to be processed")
     parser.add_argument("-K", "--upper-bound", dest="UPPER_BOUND", required=True, help="The limit for the length of the computed permutations")
     parser.add_argument("-k", "--lower-bound", dest="LOWER_BOUND", required=False, help="The lower limit for the length of the computed permutations", default=1 )
+    parser.add_argument( "-s", "--steps", dest="STEPS", default=sys.maxint, required=False, help="The number of output files to generate. Use with -S")
+    parser.add_argument( "-S", "--start", dest="START", default = 1, required=False, help="The first file to output.")
     return parser.parse_args()
 
 def main(args):
     if ( not exists("input") ): os.mkdir("input")
     infile = open(args.IN_FILE, "r")
     tests = infile.readlines()
-    create_permutations(0, int(args.LOWER_BOUND), int(args.UPPER_BOUND), tests, [], 1 )
+    create_permutations(0, int(args.LOWER_BOUND), int(args.UPPER_BOUND), tests, [], 1, int(args.START), int(args.STEPS) )
     return
 
 if __name__ == "__main__":
