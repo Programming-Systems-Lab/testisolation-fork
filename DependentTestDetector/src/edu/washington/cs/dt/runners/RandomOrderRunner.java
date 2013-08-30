2ce2130696287295e50803d7b4677ef737a964ab
racezhang@gmail.com <racezhang@gmail.com@44adb83a-8287-3463-537c-1c5ef794c0d2>
2013-08-30 21:22:45 +0000

    trivial change
    git-svn-id: http://testisolation.googlecode.com/svn/trunk@310 44adb83a-8287-3463-537c-1c5ef794c0d2
    
    Former-commit-id: aa9e55a97f29910df923b6d19df7af8abfef8194

--- DependentTestDetector/src/edu/washington/cs/dt/runners/RandomOrderRunner.java
+++ DependentTestDetector/src/edu/washington/cs/dt/runners/RandomOrderRunner.java
@@ -8,13 +8,20 @@ import java.util.Map;
 
 import edu.washington.cs.dt.OneTestExecResult;
 import edu.washington.cs.dt.TestExecResults;
+import edu.washington.cs.dt.tools.RunTestInFixedOrder;
+import edu.washington.cs.dt.util.Files;
 import edu.washington.cs.dt.util.TestExecUtils;
 import edu.washington.cs.dt.util.Utils;
 
 public class RandomOrderRunner extends AbstractTestRunner {
 
+	static int count = 0;
+	
+	private String inputTestFile;
+	
 	public RandomOrderRunner(String fileName) {
 		super(fileName);
+		this.inputTestFile = fileName;
 	}
 	
 	public RandomOrderRunner(List<String> tests) {
@@ -24,6 +31,10 @@ public class RandomOrderRunner extends AbstractTestRunner {
 	@Override
 	public TestExecResults run() {
 		List<String> randomizedList = Utils.randomList(super.junitTestList);
+//		if(RunTestInFixedOrder.verbose) {
+//			String testOrderFile = this.inputTestFile + "_order_" + (count++) + ".txt";
+//			Files.writeToFileWithNoExp(randomizedList, testOrderFile);
+//		}
 		TestExecResults result = TestExecResults.createInstance();
         Map<String, OneTestExecResult> singleRun = TestExecUtils.executeTestsInFreshJVM(super.getClassPath(),
         		super.getTmpOutputFile(), randomizedList);
