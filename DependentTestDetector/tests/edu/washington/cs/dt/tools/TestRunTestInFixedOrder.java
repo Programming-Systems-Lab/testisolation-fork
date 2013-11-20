3a7d8497c83113a29becac98a6d3344260289d44
racezhang@gmail.com <racezhang@gmail.com@44adb83a-8287-3463-537c-1c5ef794c0d2>
2013-11-20 17:13:56 +0000

    update
    git-svn-id: http://testisolation.googlecode.com/svn/trunk@456 44adb83a-8287-3463-537c-1c5ef794c0d2
    
    Former-commit-id: 2227c60aa6fccaa508b4991efb5d618cd2fa8e4f

--- DependentTestDetector/tests/edu/washington/cs/dt/tools/TestRunTestInFixedOrder.java
+++ DependentTestDetector/tests/edu/washington/cs/dt/tools/TestRunTestInFixedOrder.java
@@ -3,12 +3,35 @@
  */
 package edu.washington.cs.dt.tools;
 
+import java.util.Set;
+
+import edu.washington.cs.dt.TestExecResult;
+import edu.washington.cs.dt.TestExecResults;
+import edu.washington.cs.dt.runners.FixedOrderRunner;
 import edu.washington.cs.dt.tools.RunTestInFixedOrder;
+import edu.washington.cs.dt.util.Log;
+import edu.washington.cs.dt.util.Utils;
 import junit.framework.TestCase;
 
 public class TestRunTestInFixedOrder extends TestCase {
 	
 	public void testExamples() {
 		RunTestInFixedOrder.main(new String[]{"--testFile=./tests/edu/washington/cs/dt/main/sampleinput.txt"});
+	}
+	
+	public void testCrystal_manual() {
+		Log.logConfig("./fixed_order_crystal_results.txt");
+		
+		FixedOrderRunner fixedRunner = new FixedOrderRunner(TestRandomizedDependentTestFinder.crystalFile_manual);
+		TestExecResults expected_results = fixedRunner.run();
+		Utils.checkTrue(expected_results.getExecutionRecords().size() == 1,
+				"The size is: " + expected_results.getExecutionRecords().size());
+		TestExecResult rs = expected_results.getExecutionRecords().get(0);
+		
+		//write the results to the log file
+		Log.logln("The default execution results: ");
+		for(String test : rs.getAllTests()) {
+			Log.logln("  Test: " + test + ": " + rs.getResult(test).result);
+		}
 	}
 }
