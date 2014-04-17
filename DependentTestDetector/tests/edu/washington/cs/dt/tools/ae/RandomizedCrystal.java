c2a02ce8ee10fbd67fe51e33c856317c7fc476d5
racezhang@gmail.com <racezhang@gmail.com@44adb83a-8287-3463-537c-1c5ef794c0d2>
2014-04-17 23:42:08 +0000

    add a parameter
    git-svn-id: http://testisolation.googlecode.com/svn/trunk@533 44adb83a-8287-3463-537c-1c5ef794c0d2
    
    Former-commit-id: 19685fccec6adba078929d60b7be6f58c0c6fbe7

--- DependentTestDetector/tests/edu/washington/cs/dt/tools/ae/RandomizedCrystal.java
+++ DependentTestDetector/tests/edu/washington/cs/dt/tools/ae/RandomizedCrystal.java
@@ -5,6 +5,17 @@ import edu.washington.cs.dt.tools.TestRandomizedDependentTestFinder;
 public class RandomizedCrystal {
 
 	public static void main(String[] args) {
+		if(args.length == 1) {
+			try {
+				TestRandomizedDependentTestFinder.shuffle_times = Integer.parseInt(args[0]);
+			    System.out.println("Setting shuffle times to: "
+				    + TestRandomizedDependentTestFinder.shuffle_times);
+			} catch (Throwable e) {
+				System.out.println("It should be an integer, but " + args[0] + " is provided.");
+				return;
+			}
+		}
+		
 	       TestRandomizedDependentTestFinder finder = new TestRandomizedDependentTestFinder();
 		   finder.testCrystal_manual();
 		   finder.testCrystal_auto();
--- DependentTestDetector/tests/edu/washington/cs/dt/tools/ae/RandomizedSynoptic.java
+++ DependentTestDetector/tests/edu/washington/cs/dt/tools/ae/RandomizedSynoptic.java
@@ -5,6 +5,17 @@ import edu.washington.cs.dt.tools.TestRandomizedDependentTestFinder;
 public class RandomizedSynoptic {
 
 	public static void main(String[] args) {
+		if(args.length == 1) {
+			try {
+				TestRandomizedDependentTestFinder.shuffle_times = Integer.parseInt(args[0]);
+			    System.out.println("Setting shuffle times to: "
+				    + TestRandomizedDependentTestFinder.shuffle_times);
+			} catch (Throwable e) {
+				System.out.println("It should be an integer, but " + args[0] + " is provided.");
+				return;
+			}
+		}
+		
 	       TestRandomizedDependentTestFinder finder = new TestRandomizedDependentTestFinder();
 	       finder.testSynoptic_manual();
 		   finder.testSynoptic_auto();
